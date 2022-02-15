15f9cc5360a751209acf466986af0b3bb9731c5d
CyrilHancock <hancockcyril1910@gmail.com>
2022-02-15 18:10:35 +0530

    first commit

--- app.js
+++ app.js
@@ -7,62 +7,59 @@
     const bodyParser = require("body-parser");
     const app = express();
     //Using bod-parser
-    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
     //The public folder which holds the CSS
     app.use(express.static("public"));
     //Listening on port 3000 and if it goes well then logging a message saying that the server is running
-    app.listen(process.env.PORT||3000,function () {
-     console.log("Server is running at port 3000");
+    app.listen(process.env.PORT || 3000, function() {
+        console.log("Server is running at port 3000");
     });
     //Sending the signup.html file to the browser as soon as a request is made on localhost:3000
-    app.get("/", function (req, res) {
-     res.sendFile(__dirname + "/signup.html");
+    app.get("/", function(req, res) {
+        res.sendFile(__dirname + "/signup.html");
     });
     //Setting up MailChimp
     mailchimp.setConfig({
-    //*****************************ENTER YOUR API KEY HERE******************************
-     apiKey: "a2232172ce25779684b45bd5dc41b416-us14",
-    //*****************************ENTER YOUR API KEY PREFIX HERE i.e.THE SERVER******************************
-     server: "us14"
+        //*****************************ENTER YOUR API KEY HERE******************************
+        apiKey: "<your api key for mailchimp>",
+        //*****************************ENTER YOUR API KEY PREFIX HERE i.e.THE SERVER******************************
+        server: "<server name at the end example us-14>"
     });
     //As soon as the sign in button is pressed execute this
-    app.post("/", function (req,res) {
-    //*****************************CHANGE THIS ACCORDING TO THE VALUES YOU HAVE ENTERED IN THE INPUT ATTRIBUTE IN HTML******************************
-    const firstName = req.body.firstName;
-    console.log(firstName);
-    const secondName = req.body.secondName;
-    console.log(secondName);
-    const email = req.body.email;
-    //*****************************ENTER YOU LIST ID HERE******************************
-    const listId = "0b8658560e";
-    //Creating an object with the users data
-    const subscribingUser = {
-     firstName: firstName,
-     lastName: secondName,
-     email: email
-    };
-    //Uploading the data to the server
-     async function run() {
-    const response = await mailchimp.lists.addListMember(listId, {
-        email_address: subscribingUser.email,
-     status: "subscribed",
-     merge_fields: {
-     FNAME: subscribingUser.firstName,
-     LNAME: subscribingUser.lastName
-    }
-    });
-    //If all goes well logging the contact's id
-     res.sendFile(__dirname + "/success.html")
-     console.log(
-    `Successfully added contact as an audience member. The contact's id is ${
+    app.post("/", function(req, res) {
+        //*****************************CHANGE THIS ACCORDING TO THE VALUES YOU HAVE ENTERED IN THE INPUT ATTRIBUTE IN HTML******************************
+        const firstName = req.body.firstName;
+        console.log(firstName);
+        const secondName = req.body.secondName;
+        console.log(secondName);
+        const email = req.body.email;
+        //*****************************ENTER YOU LIST ID HERE******************************
+        const listId = "<list id >";
+        //Creating an object with the users data
+        const subscribingUser = {
+            firstName: firstName,
+            lastName: secondName,
+            email: email
+        };
+        //Uploading the data to the server
+        async function run() {
+            const response = await mailchimp.lists.addListMember(listId, {
+                email_address: subscribingUser.email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: subscribingUser.firstName,
+                    LNAME: subscribingUser.lastName
+                }
+            });
+            //If all goes well logging the contact's id
+            res.sendFile(__dirname + "/success.html")
+            console.log(
+                `Successfully added contact as an audience member. The contact's id is ${
      response.id
      }.`
-    );
-    }
-    //Running the function and catching the errors (if any)
-    // ************************THIS IS THE CODE THAT NEEDS TO BE ADDED FOR THE NEXT LECTURE*************************
-    // So the catch statement is executed when there is an error so if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
-     run().catch(e => res.sendFile(__dirname + "/failure.html"));
+            );
+        }
+        //Running the function and catching the errors (if any)
+        // So the catch statement is executed when there is an error so if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
+        run().catch(e => res.sendFile(__dirname + "/failure.html"));
     });
-
-    // 0b8658560e
